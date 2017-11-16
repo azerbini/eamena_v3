8febf1891d049ec130185c6bf360cf695ea01394
azerbini <andrea.zerbini@arch.ox.ac.uk>
2017-11-16 15:06:56 +0000

    Add list of resources to be indexed in map layers

--- eamena/eamena/settings.py
+++ eamena/eamena/settings.py
@@ -127,7 +127,8 @@ def RESOURCE_TYPE_CONFIGS():
 
 #The list of resource types which should have their geometries indexed into the maplayers index for restriction by geo area.
 MAPLAYERS_INDEX_RESOURCE_TYPES = [
-    "HERITAGE_RESOURCE_GROUP.E7"
+    "HERITAGE_RESOURCE_GROUP.E27",
+    "INFORMATION_RESOURCE.E73"
 ]
 
 #Limit number of items per Search page
